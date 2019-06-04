68983a3a04c4549d5ac09fd7fac09fe1d5ab702d
Vilija Jozaitis and cecca wrobel <vilija+cecca@>
2019-06-03 20:19:17 -0700

    set up animals page with clickable headers that toggle info paragraphs from hidden to showing and back

---
+++ js/animalScripts.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+
+  $("#snakeTitle").click(function() {
+    $("#snakeInfo").toggle();
+  });
+
+  $("#alligatorTitle").click(function() {
+    $("#alligatorInfo").toggle();
+  });
+
+  $("#dragonTitle").click(function() {
+    $("#dragonInfo").toggle();
+  });
+
+});
